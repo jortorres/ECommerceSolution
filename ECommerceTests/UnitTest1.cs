6b1cbf19837a5c094640409cf19c6aa9273f39d1
jortorres <jordan.torres1@gmail.com>
2025-10-31 19:20:33 -0500

    first commit

---
+++ ECommerceTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ECommerceTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
